54624c7ab6bd17049333e6bc43fea227e4846d39
Gaurab Kumar <137804987+GaurabCodeLab@users.noreply.github.com>
2024-01-07 17:39:42 +0530

    Update dataSlice.js

--- src/features/dataSlice.js
+++ src/features/dataSlice.js
@@ -36,3 +36,46 @@ export const usersSlice = createSlice({
 })
 
 export default usersSlice.reducer;
+
+
+// if API is calling using axios library then code is given below :-
+
+import { createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+// import axios from 'axios';
+
+// export const getData = createAsyncThunk('api call', async ()=>{
+//     try {
+//         const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+//         return response.data;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
+
+// export const usersSlice = createSlice({
+//     name : "users",
+//     initialState : {
+//         data : [],
+//         loading : false,
+//         error : null
+//     },
+//   extraReducers : (builder)=>{
+//      builder.addCase(getData.pending, (state)=>{
+//         state.loading = true;
+//      });
+//      builder.addCase(getData.fulfilled, (state, action)=>{
+//         state.loading = false;
+//         state.data = action.payload
+//      });
+//      builder.addCase(getData.rejected, (state, action)=>{
+//         state.loading = false;
+//         state.error = action.payload;
+//      })
+//   }
+// })
+
+// export default usersSlice.reducer;
+
+
+
+
