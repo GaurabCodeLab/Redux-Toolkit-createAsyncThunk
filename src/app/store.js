4ad38c4f14712c414aa29c7b1a682aa25be23678
Gaurab Kumar <137804987+GaurabCodeLab@users.noreply.github.com>
2024-01-07 17:12:40 +0530

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from '../features/dataSlice'
+import { configureStore} from '@reduxjs/toolkit';
+import rootReducer from '../features/usersSlice';
 
 const store = configureStore({
-    reducer :  {
-        api : rootReducer
-    }
+    reducer : {
+        apiData : rootReducer
+    },
+    devTools : true
 })
 
 export default store;
